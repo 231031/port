786ba524e91800599456213ca6216fd9cfba36fe
231031 <mybile.e70e@gmail.com>
2021-11-18 15:58:49 +0700

    new commit

--- one.cpp
+++ one.cpp
@@ -1,5 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    printf("esus");
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    for(i=0;i<1000;i++)
+    {
+        if(i%3==0||i%5==0)
+        k=j+i;
+        j=k;  
+    }
+    printf("%d",k);
+    
 }
