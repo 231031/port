ad610a1648cb40e7ab507e945512fc1f7dd5995e
231031 <mybile.e70e@gmail.com>
2021-11-20 20:32:31 +0700

    new commit

--- square.cpp
+++ square.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 int main()
 {
-    
+    int i;
+    int m,n,p,k,q,j=0;
+    for(i=1;i<=100;i++)
+    {
+        k = m + i*i;
+        m=k;
+        j=p+i;
+        p=j;
+    }
+    q=p*p;
+    printf("%d",q-m);
 }
