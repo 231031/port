46fae71a115e44fbad814c43e0f2c4702ffd80bd
231031 <mybile.e70e@gmail.com>
2021-11-21 20:38:12 +0700

    new commit

---
+++ 10001prime.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i=0,j=1,k,num=0;
+    while (num<10001)
+    {
+         i++;
+        k=0;
+        for (j = 1;j<=i;j++)
+        {
+            if(i%j==0) k++;  
+        } 
+        if(k==2) num++;
+    }
+    printf("%d",i);
+}
--- test.cpp
+++ test.cpp
@@ -1,14 +1,37 @@
 #include <iostream>
 using namespace std;
-int main()
-{   
-    
-	int i;
-	int t=0;
-	for (i=1;;i++)
-	{
-		if ((i%11)==0 && (i%12)==0&&(i%13)==0 && (i%14)==0 && (i%15)==0 && (i%16)==0 && (i%17)==0 && (i%18)==0 && (i%19)==0 && (i%20)==0) break;
-	}
-	 printf("%d",i);
 
+int main()
+{
+	int div, num, i;
+	
+	num = 0;
+	i = 0;
+	
+	while(true)
+	{	
+		i++;
+		div = 0;
+		
+		for(int j = 1; j <= i; j++)
+		{
+			if(i % j == 0)
+			{
+				div = div + 1;
+				
+				if(div > 2) break;
+			}
+		}
+		
+		if(div == 2)
+		{
+			num++;
+			
+			if(num == 10001)
+			{
+				cout << i;
+				break;
+			}
+		}
+	}
 }
