71b42447d21e236312ffb91f431d34735a5635d7
231031 <mybile.e70e@gmail.com>
2021-11-27 22:55:08 +0700

    new commit

--- summation_prime.cpp
+++ summation_prime.cpp
@@ -3,20 +3,25 @@
 using namespace std;
 int main()
 {
-    unsigned long long sum=0;
-    int i=1,j,k=0;
-    while(i<1999999)
+    unsigned long long sum = 0;
+    int i = 1, j, k = 0;
+    while (i < 10)
     {
-        k=0;
-        i +=2;
-        for(j=1;j<=i;j++)
+
+        i = i + 2;
+        for (j = 1; j <= i; j++)
         {
-            if(i%j==0) k++;
+            if (i % j == 0)
+                k++;
         }
-        if(k==2) sum = sum + i;
-        
-        
+        if (i>10) break;
+        if (k == 2)
+        {  
+             printf("%d\n",i); 
+            sum = sum + i;
+        }
+        k = 0;
     }
-    
-    printf("%d",sum+2);
+
+    printf("%d", sum + 2);
 }
